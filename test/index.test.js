676eaf56f9ff08af869d55f17798ac44e64591a2
Patrick Heneise <patrick@zentered.co>
2022-12-28 12:03:33 +0200

    test: adjust to fetch, remove axios from package

--- test/index.test.js
+++ test/index.test.js
@@ -2,27 +2,28 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 import { vi, test, expect } from 'vitest'
 import getDeploymentUrl from '../src/vercel.js'
-import axios from 'axios'
 
-vi.mock('axios')
+global.fetch = vi.fn()
+
+function createFetchResponse(data) {
+  return { json: () => new Promise((resolve) => resolve(data)) }
+}
 
 test('getDeploymentUrl() should return a Vercel build', async () => {
   const data = {
-    data: {
-      deployments: [
-        {
-          name: 'zentered-co',
-          url: 'test-123.vercel.app',
-          state: 'READY',
-          meta: {
-            githubCommitRef: 'fix/huge-bug',
-            githubCommitRepo: 'zentered'
-          }
+    deployments: [
+      {
+        name: 'zentered-co',
+        url: 'test-123.vercel.app',
+        state: 'READY',
+        meta: {
+          githubCommitRef: 'fix/huge-bug',
+          githubCommitRepo: 'zentered'
         }
-      ]
-    }
+      }
+    ]
   }
-  axios.get.mockResolvedValueOnce(data)
+  fetch.mockResolvedValueOnce(createFetchResponse(data))
 
   const { url, state } = await getDeploymentUrl(
     '123xyz',
@@ -30,35 +31,33 @@ test('getDeploymentUrl() should return a Vercel build', async () => {
     'fix/huge-bug',
     { teamId: 'zentered.co' }
   )
-  expect(axios.get).toBeCalledTimes(1)
+  expect(fetch).toBeCalledTimes(1)
   expect(url).toEqual('test-123.vercel.app')
   expect(state).toEqual('READY')
 })
 
 test('getDeploymentUrl() should return vercel builds for a project', async () => {
   const data = {
-    data: {
-      deployments: [
-        {
-          name: 'zentered-co',
-          url: 'test-123.vercel.app',
-          state: 'READY',
-          meta: {
-            githubCommitRef: 'fix/huge-bug',
-            githubCommitRepo: 'zentered'
-          }
+    deployments: [
+      {
+        name: 'zentered-co',
+        url: 'test-123.vercel.app',
+        state: 'READY',
+        meta: {
+          githubCommitRef: 'fix/huge-bug',
+          githubCommitRepo: 'zentered'
         }
-      ]
-    }
+      }
+    ]
   }
-  axios.get.mockResolvedValueOnce(data)
+  fetch.mockResolvedValueOnce(createFetchResponse(data))
 
   await getDeploymentUrl('123xyz', 'zentered', 'fix/huge-bug', {
     teamId: 'zentered.co',
     vercel_project_id: 'prj_zentered1'
   })
-  expect(axios.get).toBeCalledTimes(2)
-  expect(axios.get).lastCalledWith(
+  expect(fetch).toBeCalledTimes(2)
+  expect(fetch).lastCalledWith(
     'https://api.vercel.com/v6/deployments?teamId=zentered.co&vercel_project_id=prj_zentered1',
     {
       headers: {
@@ -71,11 +70,9 @@ test('getDeploymentUrl() should return vercel builds for a project', async () =>
 
 test('getDeploymentUrl() should fail if there are no deployments', async () => {
   const data = {
-    data: {
-      deployments: []
-    }
+    deployments: []
   }
-  axios.get.mockResolvedValueOnce(data)
+  fetch.mockResolvedValueOnce(createFetchResponse(data))
 
   await expect(
     getDeploymentUrl('123xyz', 'zentered', 'fix/huge-bug', {
@@ -86,21 +83,19 @@ test('getDeploymentUrl() should fail if there are no deployments', async () => {
 
 test('getDeploymentUrl() should fail if there are no matching builds', async () => {
   const data = {
-    data: {
-      deployments: [
-        {
-          name: 'zentered-co',
-          url: 'test-123.vercel.app',
-          state: 'READY',
-          meta: {
-            githubCommitRef: 'does-not-exist',
-            githubCommitRepo: 'zentered'
-          }
+    deployments: [
+      {
+        name: 'zentered-co',
+        url: 'test-123.vercel.app',
+        state: 'READY',
+        meta: {
+          githubCommitRef: 'does-not-exist',
+          githubCommitRepo: 'zentered'
         }
-      ]
-    }
+      }
+    ]
   }
-  axios.get.mockResolvedValueOnce(data)
+  fetch.mockResolvedValueOnce(createFetchResponse(data))
 
   await expect(
     getDeploymentUrl('123xyz', 'zentered', 'fix/huge-bug', {
